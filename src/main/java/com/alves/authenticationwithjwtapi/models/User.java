34c54694e5448366b64a9247425273dda6d1b1f2
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-12 18:55:20 -0300

    User implements UserDetails from Spring Security

--- src/main/java/com/alves/authenticationwithjwtapi/models/User.java
+++ src/main/java/com/alves/authenticationwithjwtapi/models/User.java
@@ -1,6 +1,10 @@
 package com.alves.authenticationwithjwtapi.models;
 
+import java.util.Collection;
 import java.util.UUID;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,7 +17,7 @@ import lombok.Setter;
 @Getter @Setter
 @Table(name = "`user`")
 @Entity
-public class User {
+public class User implements UserDetails {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,5 +27,30 @@ public class User {
   private String password;
   private UUID verificationCode;
   private boolean enabled;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return null;
+  }
+
+  @Override
+  public String getUsername() {
+    return email;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
   
 }
