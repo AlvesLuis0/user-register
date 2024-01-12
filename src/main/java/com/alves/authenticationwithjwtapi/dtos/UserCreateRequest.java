0eb71228460ff70c172380f990c15bce7768b3b2
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-12 19:44:22 -0300

    Validation dependency added

--- src/main/java/com/alves/authenticationwithjwtapi/dtos/UserCreateRequest.java
+++ src/main/java/com/alves/authenticationwithjwtapi/dtos/UserCreateRequest.java
@@ -2,7 +2,24 @@ package com.alves.authenticationwithjwtapi.dtos;
 
 import com.alves.authenticationwithjwtapi.models.User;
 
-public record UserCreateRequest(String name, String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserCreateRequest(
+
+  @NotBlank(message = "Name is mandatory.")
+  String name,
+
+  @NotBlank(message = "Email is mandatory.")
+  @Email
+  String email,
+
+  @NotBlank(message = "Password is mandatory.")
+  @Size(min = 8, message = "The password must contain at least 8 characters")
+  String password
+
+) {
 
   public User toModel() {
     User user = new User();
