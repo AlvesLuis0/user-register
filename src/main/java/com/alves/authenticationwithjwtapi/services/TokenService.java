62b4c20b306abbe824cc4542bb7de682ffe8149f
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-14 02:01:43 -0300

    TokenService created

---
+++ src/main/java/com/alves/authenticationwithjwtapi/services/TokenService.java
@@ -0,0 +1,56 @@
+package com.alves.authenticationwithjwtapi.services;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.alves.authenticationwithjwtapi.models.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+
+@Service
+public class TokenService {
+
+  private final String ISSUER = "authentication-with-jwt-api";
+  private final Algorithm ALGORITHM;
+
+  public TokenService(@Value("jwt.secret") String SECRET) {
+    this.ALGORITHM = Algorithm.HMAC256(SECRET);
+  }
+
+  public String generateToken(User user) {
+    try {
+      return JWT.create()
+        .withIssuer(ISSUER)
+        .withSubject(user.getEmail())
+        .withExpiresAt(getExpirationDate())
+        .sign(ALGORITHM);
+    }
+    catch(JWTCreationException exception) {
+      throw new RuntimeException("Error while creating token.", exception);
+    }
+  }
+
+  public String validateToken(String token) {
+    try {
+      return JWT.require(ALGORITHM)
+        .withIssuer(ISSUER)
+        .build()
+        .verify(token)
+        .getSubject();
+    }
+    catch(JWTVerificationException exception) {
+      throw new RuntimeException("Invalid token.");
+    }
+  }
+
+  private Instant getExpirationDate() {
+    return LocalDateTime.now().plusMinutes(5).toInstant(ZoneOffset.of("-03:00"));
+  }
+  
+}
