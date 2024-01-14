e16665755ab759ca0258e9f3f7a071f6c10ca5be
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-14 00:26:32 -0300

    MailService created

---
+++ src/main/java/com/alves/authenticationwithjwtapi/services/MailService.java
@@ -0,0 +1,23 @@
+package com.alves.authenticationwithjwtapi.services;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class MailService {
+
+  private final JavaMailSender javaMailSender;
+
+  public void sendEmail(String to, String subject, String text) {
+    SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+    simpleMailMessage.setTo(to);
+    simpleMailMessage.setSubject(subject);
+    simpleMailMessage.setText(text);
+    javaMailSender.send(simpleMailMessage);
+  }
+
+}
